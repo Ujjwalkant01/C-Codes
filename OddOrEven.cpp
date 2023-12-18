e508866fab873627649660743c1c433bbedeed08
Ujjwal kant <131240250+Ujjwalkant01@users.noreply.github.com>
2023-12-18 10:14:09 +0530

    Add files via upload

---
+++ OddOrEven.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the value of n:\n";
+    cin>>n;
+    if(n%2==0){
+        cout<<"Even number\n";
+
+    }
+    else{
+        cout<<"Odd number";
+    }
+}
---
+++ SumOfTwoNo.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    int m;
+    int sum;
+    cout<<"Enter the value of n:\n";
+    cin>>n;
+    
+    cout<<"Enter the value of m:\n";
+    cin>>m;
+
+
+    cout<<"Sun of n and m is:\n"<<n+m;
+
+
+}
